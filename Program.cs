af0b79bb2a9167a8aa3ae1c9fd96458aca3337b7
yemrekaradag <yemrekaradag@hotmail.com>
2024-05-25 22:45:28 +0300

    Message Queue class and API Versioning added.

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using OnlineBankingApplication.Context;
 using System.Text;
 
@@ -30,7 +31,10 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "OnlineBankingApp", Version = "v1" });
+});
 
 var app = builder.Build();
 
---
+++ Services/MessageQueueService.cs
@@ -0,0 +1,25 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+namespace OnlineBankingApplication.Services
+{
+    public class MessageQueueService
+    {
+        private readonly IConnection _connection;
+        private readonly IModel _channel;
+
+        public MessageQueueService()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            _connection = factory.CreateConnection();
+            _channel = _connection.CreateModel();
+            _channel.QueueDeclare(queue: "transactions", durable: false, exclusive: false, autoDelete: false, arguments: null);
+        }
+
+        public void Publish(string message)
+        {
+            var body = Encoding.UTF8.GetBytes(message);
+            _channel.BasicPublish(exchange: "", routingKey: "transactions", basicProperties: null, body: body);
+        }
+    }
+}
