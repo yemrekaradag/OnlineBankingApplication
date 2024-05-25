7eae5a59cdaa4dae3c28109efc74f6368d59ee13
yemrekaradag <yemrekaradag@hotmail.com>
2024-05-25 22:37:34 +0300

    Added Circuit Breaker polly

---
+++ Services/ResilientHttpClient.cs
@@ -0,0 +1,23 @@
+﻿using Polly.CircuitBreaker;
+using Polly;
+
+namespace OnlineBankingApplication.Services
+{
+    public class ResilientHttpClient
+    {
+        private readonly AsyncCircuitBreakerPolicy<HttpResponseMessage> _circuitBreakerPolicy;
+
+        public ResilientHttpClient()
+        {
+            _circuitBreakerPolicy = Policy
+                .HandleResult<HttpResponseMessage>(r => !r.IsSuccessStatusCode)
+                .CircuitBreakerAsync(2, TimeSpan.FromMinutes(1));
+        }
+
+        public async Task<HttpResponseMessage> GetAsync(string uri)
+        {
+            using var httpClient = new HttpClient();
+            return await _circuitBreakerPolicy.ExecuteAsync(() => httpClient.GetAsync(uri));
+        }
+    }
+}
