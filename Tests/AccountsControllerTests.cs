76c0fa55d7448eb347c54c05b2c50d3a7c809ee2
yemrekaradag <yemrekaradag@hotmail.com>
2024-05-25 23:15:57 +0300

    Added xunit and docker container

---
+++ Tests/AccountsControllerTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using OnlineBankingApplication.Context;
+using OnlineBankingApplication.Controllers;
+using OnlineBankingApplication.Entities;
+using Xunit;
+
+namespace OnlineBankingApplication.Tests
+{
+    public class AccountsControllerTests
+    {
+        [Fact]
+        public async Task CreateAccount_ReturnsCreatedAtActionResult()
+        {
+            // Arrange
+            var mockContext = new Mock<BaseDbContext>();
+            var controller = new AccountsController(mockContext.Object);
+
+            // Act
+            var result = await controller.CreateAccount(new Account());
+
+            // Assert
+            Assert.IsType<CreatedAtActionResult>(result);
+        }
+    }
+}
